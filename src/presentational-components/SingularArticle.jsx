f40c5be31431de5ada6b2cacf6fef6b605f889ee
Alexander Scharpf <alexander.scharpf94@gmx.de>
2023-03-16 21:59:09 +0100

    Add: First commit

---
+++ src/presentational-components/SingularArticle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingularArticle() {
+  return (
+    <img className='test-image' src='https://content.moss.co.uk/images/extralarge/966576201_01.jpg' alt='Random Model' />
+  )
+}
+
+export default SingularArticle
