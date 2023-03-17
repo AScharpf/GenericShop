ea3b22019dac9153ec577336dc9208dbf2c53709
Alexander Scharpf <alexander.scharpf94@gmx.de>
2023-03-17 18:27:13 +0100

    Add: Playing with navbar

---
+++ src/presentational-components/DropDownMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DropDownMenu() {
+  return (
+    <div>DropDownMenu</div>
+  )
+}
+
+export default DropDownMenu
--- src/presentational-components/Navbar.jsx
+++ src/presentational-components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import DropDownMenu from './DropDownMenu'
 
 export default function Navbar() {
   return (
-    <div className='nav-bar-top'></div>
+    <div className='nav-bar-top'>
+    <DropDownMenu />
+      <span className='shop-title'>
+        <h1>Generic Shop</h1>
+      </span>
+      <span className='shopping-basket'>
+        <button>🛒</button>
+      </span>
+    </div>
   )
 }
